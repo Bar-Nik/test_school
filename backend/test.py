2c94feb3429e76652bdb5be6387a56a2da6846fc
ehristenko <>
2024-04-10 10:17:32 +0300

    add ConfigManager LogManager

---
+++ backend/test.py
@@ -0,0 +1,14 @@
+import os
+from app.Managers.ConfigManager import ConfigManager
+from app.Managers.LogManager import LogManager
+
+
+
+DIR = os.path.dirname(__file__)
+
+
+config_manager = ConfigManager(DIR)
+log_manager = LogManager(config_manager)
+
+
+log_manager.warning("EROORRRR!")
